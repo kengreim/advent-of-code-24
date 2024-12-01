1c1c5265ba2bbac350bf523a0a82a15140bbd1a4
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-01 10:03:57 -0800

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+
+    let file = File::open("day1/src/day1_input.txt").expect("file not found");
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            let splits = line.split_whitespace().collect::<Vec<_>>();
+            list1.push(splits[0].parse::<i32>().unwrap());
+            list2.push(splits[1].parse::<i32>().unwrap());
+        }
+    }
+
+    list1.sort();
+    list2.sort();
+
+    // Part 1
+    let result = list1
+        .iter()
+        .zip(list2.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum::<i32>();
+    println!("result: {}", result);
+
+    // Part 2
+    let mut list1_map = HashMap::new();
+    for i in list1.iter() {
+        list1_map.insert(*i, 0);
+    }
+
+    for i in list2.iter() {
+        if let Some(val) = list1_map.get(i) {
+            list1_map.insert(*i, val + 1);
+        }
+    }
+
+    let mut sum = 0;
+    for i in list1.iter() {
+        if let Some(val) = list1_map.get(i) {
+            sum += i * val;
+        }
+    }
+    println!("result: {}", sum);
+}
