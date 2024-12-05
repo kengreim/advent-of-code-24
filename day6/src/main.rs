4c542904b53e83fa15e32a5f00806176c77f1b77
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-05 12:14:35 -0800

    Day 6 setup

---
+++ day6/src/main.rs
@@ -0,0 +1,11 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    const PATH: &str = "day6/src/day6_input.txt";
+
+    // Part 1
+    let input = fs::read_to_string(PATH).unwrap();
+}
