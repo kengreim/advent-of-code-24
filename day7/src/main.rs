0c31992a94e9a09b91f4646656251172b1ed4ba8
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-07 07:30:01 -0800

    Added recursive version with pruning

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,9 +1,12 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use std::fs;
+use std::time::Instant;
 
 fn main() {
-    part2()
+    let start = Instant::now();
+    part2_recursive();
+    println!("{:?}", start.elapsed());
 }
 
 fn part1() {
@@ -48,6 +51,21 @@ fn part2() {
     println!("{sum}");
 }
 
+fn part2_recursive() {
+    const PATH: &str = "day7/src/day7_input.txt";
+
+    // Part 1
+    let input = fs::read_to_string(PATH).unwrap();
+    let mut sum = 0;
+    for line in input.lines() {
+        let (total, seq) = parse_line(line);
+        if can_be_valid(total, &seq, seq.len() - 1) {
+            sum += total;
+        }
+    }
+    println!("{sum}");
+}
+
 fn parse_line(line: &str) -> (i64, Vec<i64>) {
     if let Some((total, nums)) = line.split_once(':') {
         (
@@ -77,4 +95,39 @@ fn calc_possibilities_part2(possible_totals: &[i64], next_num: i64) -> Vec<i64> 
 
 fn concat_nums(a: i64, b: i64) -> i64 {
     format!("{a}{b}").parse().unwrap()
+}
+
+fn can_be_valid(total: i64, seq: &[i64], idx: usize) -> bool {
+    if idx == 1 {
+        total.to_string() == format!("{}{}", seq[0], seq[1])
+            || total == seq[0] + seq[1]
+            || total == seq[0] * seq[1]
+    } else {
+        let mut possibilties = vec![];
+
+        if let Some(s) = unconcat(&total.to_string(), &seq[idx].to_string()) {
+            possibilties.push((&seq[0..idx], idx - 1, s.parse::<i64>().unwrap()));
+        }
+
+        if total % seq[idx] == 0 {
+            possibilties.push((&seq[0..idx], idx - 1, total / seq[idx]))
+        }
+
+        if total - seq[idx] > 0 {
+            possibilties.push((&seq[0..idx], idx - 1, total - seq[idx]));
+        }
+
+        possibilties
+            .iter()
+            .map(|(seq, idx, total)| can_be_valid(*total, seq, *idx))
+            .any(|b| b)
+    }
+}
+
+fn unconcat(full_str: &str, end_str: &str) -> Option<String> {
+    if full_str.ends_with(end_str) && full_str != end_str {
+        Some(full_str[0..full_str.len() - end_str.len()].to_string())
+    } else {
+        None
+    }
 }
