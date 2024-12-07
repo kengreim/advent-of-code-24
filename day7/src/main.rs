3d2c931fcc1d95351c4717d935278f7b3b36269f
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-06 22:12:02 -0800

    Day7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,7 +3,7 @@
 use std::fs;
 
 fn main() {
-    part1()
+    part2()
 }
 
 fn part1() {
@@ -14,12 +14,32 @@ fn part1() {
     let mut sum = 0;
     for line in input.lines() {
         let (total, seq) = parse_line(line);
-        let initial_possibilities = calc_possibilities(&[seq[0]], seq[1]);
+        let initial_possibilities = calc_possibilities_part1(&[seq[0]], seq[1]);
 
-        let all_possibilities = seq
-            .iter()
-            .skip(2)
-            .fold(initial_possibilities, |acc, n| calc_possibilities(&acc, *n));
+        let all_possibilities = seq.iter().skip(2).fold(initial_possibilities, |acc, n| {
+            calc_possibilities_part1(&acc, *n)
+        });
+
+        if all_possibilities.contains(&total) {
+            sum += total;
+        }
+    }
+    println!("{sum}");
+}
+
+fn part2() {
+    const PATH: &str = "day7/src/day7_input.txt";
+
+    // Part 1
+    let input = fs::read_to_string(PATH).unwrap();
+    let mut sum = 0;
+    for line in input.lines() {
+        let (total, seq) = parse_line(line);
+        let initial_possibilities = calc_possibilities_part2(&[seq[0]], seq[1]);
+
+        let all_possibilities = seq.iter().skip(2).fold(initial_possibilities, |acc, n| {
+            calc_possibilities_part2(&acc, *n)
+        });
 
         if all_possibilities.contains(&total) {
             sum += total;
@@ -41,9 +61,20 @@ fn parse_line(line: &str) -> (i64, Vec<i64>) {
     }
 }
 
-fn calc_possibilities(possible_totals: &[i64], next_num: i64) -> Vec<i64> {
+fn calc_possibilities_part1(possible_totals: &[i64], next_num: i64) -> Vec<i64> {
     possible_totals
         .iter()
         .flat_map(|t| vec![t + next_num, t * next_num])
         .collect()
+}
+
+fn calc_possibilities_part2(possible_totals: &[i64], next_num: i64) -> Vec<i64> {
+    possible_totals
+        .iter()
+        .flat_map(|t| vec![t + next_num, t * next_num, concat_nums(*t, next_num)])
+        .collect()
+}
+
+fn concat_nums(a: i64, b: i64) -> i64 {
+    format!("{a}{b}").parse().unwrap()
 }
