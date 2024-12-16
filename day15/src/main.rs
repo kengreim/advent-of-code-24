e16c0ed076218318744a72519ea5215235baf836
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-15 17:38:55 -0800

    Day 15 part 2 testing

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -4,8 +4,10 @@ use grid::Grid;
 use std::cmp::min;
 use utils::GridExt;
 
+type WideCell = (usize, (usize, usize));
+
 fn main() {
-    const PATH: &str = "day15/src/day15_example.txt";
+    const PATH: &str = "day15/src/day15_test.txt";
     //part1(PATH);
     part2(PATH);
 }
@@ -82,50 +84,79 @@ fn part2(path: &str) {
                 }
                 '^' | 'v' => {
                     let (next_row, next_col) = next_cell;
+                    let mut left_aligned: bool;
+                    let mut widebox: WideCell;
 
-                    let (min_check_col, max_check_col) = if next_val.unwrap() == '[' {
-                        (next_col, next_col + 1)
+                    if next_val.unwrap() == '[' {
+                        widebox = (next_row, (next_col, next_col + 1));
+                        left_aligned = true;
                     } else {
-                        (next_col - 1, next_col)
-                    };
+                        widebox = (next_row, (next_col - 1, next_col));
+                        left_aligned = false;
+                    }
+
+                    // let (min_check_col, max_check_col) = if next_val.unwrap() == '[' {
+                    //     (next_col, next_col + 1)
+                    // } else {
+                    //     (next_col - 1, next_col)
+                    // };
                     //println!("{min_check_col} {max_check_col}");
 
                     let is_increasing_row = m == 'v';
 
                     let test_grid = grid.clone();
 
-                    if let Some(((free_row, min_col_free), (_, max_col_free))) = next_free_wide(
-                        &grid,
-                        next_cell,
-                        is_increasing_row,
-                        (min_check_col, max_check_col),
-                    ) {
-                        let mut move_row = free_row;
-                        while move_row != next_row {
-                            let offset_row = if is_increasing_row {
-                                move_row - 1
-                            } else {
-                                move_row + 1
-                            };
-                            for col in min_col_free..=max_col_free {
-                                println!("{move_row} {col} comes from {offset_row} {col}");
-                                grid[(move_row, col)] = grid[(offset_row, col)];
-                            }
-
-                            move_row = if is_increasing_row {
-                                move_row - 1
-                            } else {
-                                move_row + 1
-                            };
+                    if let Some(mut boxes) =
+                        next_free_wide(&grid, next_cell, is_increasing_row, vec![widebox], vec![])
+                    {
+                        println!("Found some boxes {:?}", boxes);
+                        if is_increasing_row {
+                            boxes.sort_by(|a, b| b.0.cmp(&a.0));
+                        } else {
+                            boxes.sort_by(|a, b| a.0.cmp(&b.0));
                         }
 
-                        if grid[next_cell] == ']' {
-                            grid[(next_row, next_col - 1)] = '.';
-                        } else if grid[next_cell] == '[' {
-                            grid[(next_row, next_col + 1)] = '.';
+                        for (row, (c1, c2)) in boxes {
+                            let offset_row = if is_increasing_row { row - 1 } else { row + 1 };
+
+                            //grid.print();
+                            //println!("writing {offset_row} {c1} {c2} to {row} {c1} {c2}");
+                            grid[(row, c1)] = grid[(offset_row, c1)];
+                            grid[(row, c2)] = grid[(offset_row, c2)];
                         }
+
+                        // let mut move_row = free_row;
+                        // while move_row != next_row {
+                        //     let offset_row = if is_increasing_row {
+                        //         move_row - 1
+                        //     } else {
+                        //         move_row + 1
+                        //     };
+                        //     for col in min_col_free..=max_col_free {
+                        //         println!("{move_row} {col} comes from {offset_row} {col}");
+                        //         grid[(move_row, col)] = grid[(offset_row, col)];
+                        //     }
+                        //
+                        //     move_row = if is_increasing_row {
+                        //         move_row - 1
+                        //     } else {
+                        //         move_row + 1
+                        //     };
+                        // }
+
+                        //println!("{:?}", next_cell);
+                        // if grid[next_cell] == '@' {
+                        //     grid[(next_row, next_col + 1)] = '.';
+                        // } else {
+                        //     grid[(next_row, next_col + 1)] = '.';
+                        // }
 
                         grid[next_cell] = '@';
+                        if left_aligned {
+                            grid[(next_row, next_col + 1)] = '.';
+                        } else {
+                            grid[(next_row, next_col - 1)] = '.';
+                        }
                         grid[pos] = '.';
                         pos = next_cell;
                     }
@@ -198,35 +229,73 @@ fn next_free_wide(
     grid: &Grid<char>,
     (row, col): (usize, usize),
     is_increasing_row: bool,
-    (mut min_check_col, mut max_check_col): (usize, usize),
-) -> Option<((usize, usize), (usize, usize))> {
-    let chars = (min_check_col..=max_check_col)
-        .filter_map(|col| grid.get(row, col).copied())
-        .collect::<Vec<_>>();
-
-    if chars.iter().any(|c| *c == '#') {
+    //(mut min_check_col, mut max_check_col): (usize, usize),
+    frontier_row: Vec<WideCell>,
+    mut acc_boxes: Vec<WideCell>,
+) -> Option<Vec<WideCell>> {
+    if frontier_row.iter().any(|(row, (col1, col2))| {
+        *grid.get(*row, *col1).unwrap() == '#' || *grid.get(*row, *col2).unwrap() == '#'
+    }) {
         None
-    } else if chars.iter().all(|c| *c == '.') {
-        Some(((row, min_check_col), (row, max_check_col)))
+    } else if frontier_row.iter().all(|(row, (col1, col2))| {
+        *grid.get(*row, *col1).unwrap() == '.' && *grid.get(*row, *col2).unwrap() == '.'
+    }) {
+        acc_boxes.extend(frontier_row);
+        Some(acc_boxes)
     } else {
+        acc_boxes.extend(frontier_row.clone());
+
+        let mut new_frontier = vec![];
+
         let next_row = if is_increasing_row { row + 1 } else { row - 1 };
-        min_check_col = if *chars.first().unwrap() == ']' {
-            min_check_col - 1
-        } else {
-            min_check_col
-        };
-        max_check_col = if *chars.last().unwrap() == '[' {
-            max_check_col + 1
-        } else {
-            max_check_col
-        };
+
+        for (_, (col1, col2)) in frontier_row {
+            new_frontier.push((next_row, (col1, col2)));
+            //println!("{next_row} {col1} {col2}");
+            if *grid.get(next_row, col1).unwrap() == ']' {
+                new_frontier.push((next_row, (col1 - 1, col1)))
+            }
+            if *grid.get(next_row, col2).unwrap() == '[' {
+                new_frontier.push((next_row, (col2, col2 + 1)))
+            }
+        }
+
         next_free_wide(
             grid,
             (next_row, col),
             is_increasing_row,
-            (min_check_col, max_check_col),
+            new_frontier,
+            acc_boxes,
         )
     }
+
+    // let chars = (min_check_col..=max_check_col)
+    //     .filter_map(|col| grid.get(row, col).copied())
+    //     .collect::<Vec<_>>();
+    //
+    // if chars.iter().any(|c| *c == '#') {
+    //     None
+    // } else if chars.iter().all(|c| *c == '.') {
+    //     Some(((row, min_check_col), (row, max_check_col)))
+    // } else {
+    //     let next_row = if is_increasing_row { row + 1 } else { row - 1 };
+    //     min_check_col = if *chars.first().unwrap() == ']' {
+    //         min_check_col - 1
+    //     } else {
+    //         min_check_col
+    //     };
+    //     max_check_col = if *chars.last().unwrap() == '[' {
+    //         max_check_col + 1
+    //     } else {
+    //         max_check_col
+    //     };
+    //     next_free_wide(
+    //         grid,
+    //         (next_row, col),
+    //         is_increasing_row,
+    //         (min_check_col, max_check_col),
+    //     )
+    // }
 
     // loop {
     //     let chars = (min_check_col..=max_check_col)
