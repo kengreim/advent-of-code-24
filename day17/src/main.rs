ed46b525ac569538f5439ecf39daea26e44c42a6
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-16 22:29:52 -0800

    Day 17 brute force ready

---
+++ day17/src/main.rs
@@ -0,0 +1,169 @@
+use regex::Regex;
+use std::fs;
+use std::sync::LazyLock;
+
+static REGISTER_A_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"Register A: (-?\d+)").unwrap());
+static REGISTER_B_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"Register B: (-?\d+)").unwrap());
+static REGISTER_C_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"Register C: (-?\d+)").unwrap());
+
+static PROGRAM_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"Program: (.+)").unwrap());
+
+fn main() {
+    const PATH: &str = "day17/src/day17_input.txt";
+
+    //part1(PATH);
+    part2(PATH);
+}
+
+fn part2(path: &str) {
+    let input = fs::read_to_string(&path).unwrap();
+    let (program, (_, register_b, register_c)) = parse_program(&input);
+
+    let mut register_a = 0;
+    loop {
+        if register_a % 10_000_000 == 0 {
+            println!("Trying {}", register_a);
+        }
+
+        let output = execute_program(&program, (register_a, register_b, register_c));
+        if output == program {
+            println!("{register_a}");
+            break;
+        }
+
+        register_a += 1;
+    }
+}
+
+fn part1(path: &str) {
+    let input = fs::read_to_string(&path).unwrap();
+    let (program, registers) = parse_program(&input);
+    let output = execute_program(&program, registers)
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<Vec<_>>();
+    println!("{}", output.join(","));
+}
+
+fn parse_program(input: &str) -> (Vec<u32>, (u32, u32, u32)) {
+    let register_a: u32 = REGISTER_A_RE
+        .captures(&input)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .parse()
+        .unwrap();
+    let register_b: u32 = REGISTER_B_RE
+        .captures(&input)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .parse()
+        .unwrap();
+    let register_c: u32 = REGISTER_C_RE
+        .captures(&input)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .parse()
+        .unwrap();
+
+    let program_captures = PROGRAM_RE.captures(&input).unwrap();
+    let program = program_captures
+        .get(1)
+        .unwrap()
+        .as_str()
+        .split(",")
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    (program, (register_a, register_b, register_c))
+}
+
+fn execute_program(program: &[u32], registers: (u32, u32, u32)) -> Vec<u32> {
+    let (mut register_a, mut register_b, mut register_c) = registers;
+    let mut output = vec![];
+
+    let mut i_pointer = 0;
+    while i_pointer < program.len() {
+        let opcode = program[i_pointer];
+        let operand = program[i_pointer + 1];
+
+        match opcode {
+            0 => {
+                let numerator = register_a;
+                let denominator = 2_u32.pow(get_combo_operand_val(
+                    operand,
+                    (register_a, register_b, register_c),
+                ));
+                register_a = numerator / denominator;
+                i_pointer += 2;
+            }
+            1 => {
+                let new = register_b ^ operand;
+                register_b = new;
+                i_pointer += 2;
+            }
+            2 => {
+                let new = get_combo_operand_val(operand, (register_a, register_b, register_c)) % 8;
+                register_b = new;
+                i_pointer += 2;
+            }
+            3 => {
+                if register_a != 0 {
+                    i_pointer = operand as usize;
+                } else {
+                    i_pointer += 2;
+                }
+            }
+            4 => {
+                let new = register_b ^ register_c;
+                register_b = new;
+                i_pointer += 2;
+            }
+            5 => {
+                let new_output =
+                    get_combo_operand_val(operand, (register_a, register_b, register_c)) % 8;
+                output.push(new_output);
+                i_pointer += 2;
+            }
+            6 => {
+                let numerator = register_a;
+                let denominator = 2_u32.pow(get_combo_operand_val(
+                    operand,
+                    (register_a, register_b, register_c),
+                )) as u32;
+                register_b = numerator / denominator;
+                i_pointer += 2;
+            }
+            7 => {
+                let numerator = register_a;
+                let denominator = 2_u32.pow(get_combo_operand_val(
+                    operand,
+                    (register_a, register_b, register_c),
+                )) as u32;
+                register_c = numerator / denominator;
+                i_pointer += 2;
+            }
+            _ => panic!("Unknown opcode {opcode}"),
+        }
+    }
+
+    output
+}
+
+fn get_combo_operand_val(operand: u32, registers: (u32, u32, u32)) -> u32 {
+    match operand {
+        0 | 1 | 2 | 3 => operand,
+        4 => registers.0,
+        5 => registers.1,
+        6 => registers.2,
+        _ => panic!("Unknown combo operand {operand}"),
+    }
+}
