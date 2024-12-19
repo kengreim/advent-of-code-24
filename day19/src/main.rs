13bc18f012d1123eed09c576e95cee3b03aa256f
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-12-19 10:48:40 -0800

    Day 19 P 1 complete

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::sync::{Arc, RwLock};
 
 fn main() {
-    const PATH: &str = "day19/src/day19_input_test.txt";
+    const PATH: &str = "day19/src/day19_input.txt";
     part1(PATH);
 }
 
@@ -21,92 +21,52 @@ fn part1(path: &str) {
         .collect::<Vec<_>>();
     let designs = &lines[2..];
 
-    let memo_set = Arc::new(RwLock::new(
+    let designs_memo = Arc::new(RwLock::new(
         towels
             .iter()
             .map(|&s| (s.to_owned(), true))
             .collect::<FxHashMap<String, bool>>(),
     ));
 
-    let mut sum = 0;
-    for &d in designs {
-        //println!("try {d}");
-        if can_build_design(d, "", memo_set.clone()) {
-            sum += 1;
-        }
-    }
-
-    // let sum = designs
-    //     .iter()
-    //     .map(|&d| can_build_design(d, &towels, memo_set.clone()))
-    //     .filter(|b| *b)
-    //     .count();
+    let sum = designs
+        .iter()
+        .map(|&d| can_build_design(d, &designs_memo))
+        .filter(|b| *b)
+        .count();
 
     println!("{sum}");
 }
 
-fn can_build_design(
-    design: &str,
-    remainder: &str,
-    towels_memo: Arc<RwLock<FxHashMap<String, bool>>>,
-) -> bool {
-    let previous_res = towels_memo.read().unwrap().get(design).cloned();
-    if let Some(res) = previous_res {
-        //memo_set.write().unwrap().insert(design.to_string());
-        //println!("found {design} in memo");
-        res
-    } else {
-        // println!(
-        //     "{:?}",
-        //     towels_memo.read().unwrap().keys().collect::<Vec<_>>()
-        // );
-        let candidates = towels_memo
-            .read()
-            .unwrap()
-            .keys()
-            .filter_map(|s| {
-                //println!("trying {}", s);
-                if design.ends_with(s.as_str()) {
-                    let new_len = design.len() - s.len();
-                    let remainder = format!("{}{}", design[new_len..].to_owned(), remainder);
-                    //println!("ends with {s} in memo");
-                    Some((design[..new_len].to_owned(), remainder))
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
+fn can_build_design(design: &str, designs_memo: &Arc<RwLock<FxHashMap<String, bool>>>) -> bool {
+    let previous_res = designs_memo.read().unwrap().get(design).copied();
+    previous_res.map_or_else(
+        || {
+            let candidates = designs_memo
+                .read()
+                .unwrap()
+                .keys()
+                .filter_map(|s| {
+                    if design.ends_with(s.as_str()) {
+                        let new_len = design.len() - s.len();
+                        Some(design[..new_len].to_owned())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
 
-        //println! {"{:?}", candidates};
+            let res = candidates
+                .iter()
+                .any(|left_str| can_build_design(left_str, designs_memo));
 
-        let mut write_lock = towels_memo.write().unwrap();
-        for (left_str, remainder) in &candidates {
-            //println!("candidate {left_str} and writing remainder {remainder}");
+            designs_memo
+                .write()
+                .unwrap()
+                .entry(design.to_string())
+                .or_insert(res);
 
-            write_lock.entry(remainder.to_owned()).or_insert(true);
-
-            //write_lock.insert(remainder.clone());
-        }
-        drop(write_lock);
-
-        //println!("done");
-
-        //println!("{design} {:?}", candidates);
-
-        let res = candidates.iter().any(|(left_str, remainder)| {
-            //println!("call can build {left_str} {remainder}");
-            can_build_design(left_str, remainder, towels_memo.clone())
-        });
-        towels_memo
-            .write()
-            .unwrap()
-            .entry(design.to_string())
-            .or_insert(res);
-        //
-        // if res {
-        //     println!("writing {design}");
-        // }
-
-        res
-    }
+            res
+        },
+        |res| res,
+    )
 }
